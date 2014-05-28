7d5b0baa66844669df307c6885cacc2f4add0b8c
jie-zhang <jie-zhang@rontech.co.jp>
2014-05-28 08:45:55 +0800

    Initialize the project for Meifan Platform Framework.
      * commons for all other project of Meifan.

---
+++ app/com/meifannet/framework/auth/BaseAuth.scala
@@ -0,0 +1,44 @@
+/**
+ * RONTECH CONFIDENTIAL
+ * __________________
+ *
+ *  [2014] - SuZhou Rontech Co.,Ltd.(http://www.sz-rontech.com)
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of SuZhou Rontech Co.,Ltd. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to SuZhou Rontech Co.,Ltd.
+ * and its suppliers and may be covered by China and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from SuZhou Rontech Co.,Ltd..
+ */
+package com.meifannet.framework.auth
+
+import reflect.{ ClassTag, classTag }
+import play.api.mvc._
+import play.api.mvc.Results._
+import scala.concurrent.{ ExecutionContext, Future }
+
+import jp.t2v.lab.play2.auth.AuthConfig
+import jp.t2v.lab.play2.auth.AuthElement
+import jp.t2v.lab.play2.auth.LoginLogout
+import jp.t2v.lab.play2.auth.OptionalAuthElement
+
+/**
+ * trait for the authorization and authentication of salon user.
+ *
+ * @since 1.0
+ * @see jp.t2v.lab.play2.auth.AuthConfig
+ */
+trait BaseAuthConfig extends AuthConfig {}
+
+trait BaseAuthElement extends AuthElement {}
+
+trait BaseLoginLogout extends LoginLogout {}
+
+trait BaseOptionalAuthElement extends OptionalAuthElement {}
+
+
---
+++ project/build.scala
@@ -0,0 +1,26 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "framework"
+  val appVersion      = "1.0-SNAPSHOT"
+
+  val appDependencies = Seq(
+    "se.radley" %% "play-plugins-salat" % "1.3.0",
+    //ADD BY YS-HAN 2014-03-20 FOR AUTH
+    "jp.t2v" %% "play2-auth"      % "0.11.0",
+    "jp.t2v" %% "play2-auth-test" % "0.11.0" % "test",
+
+    "org.mindrot" % "jbcrypt" % "0.3m",
+
+    // Add by wu for mail
+    "com.typesafe" %% "play-plugins-mailer" % "2.2.0"
+  )
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    routesImport += "se.radley.plugin.salat.Binders._",
+    templatesImport += "org.bson.types.ObjectId"
+  )
+}
